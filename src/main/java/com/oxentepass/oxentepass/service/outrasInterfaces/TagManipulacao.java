df44bdb34203b4d2061b2cbc54ded1637703b0e6
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-12-11 20:26:44 -0300

    refactor(#10): renomeando parametros em TagManipulacao

--- src/main/java/com/oxentepass/oxentepass/service/outrasInterfaces/TagManipulacao.java
+++ src/main/java/com/oxentepass/oxentepass/service/outrasInterfaces/TagManipulacao.java
@@ -3,7 +3,7 @@ package com.oxentepass.oxentepass.service.outrasInterfaces;
 import com.oxentepass.oxentepass.entity.Tag;
 
 public interface TagManipulacao {
-    public void adicionarTagExistente(long idEvento, long idTag); // Adição de Tag existente
-    public void adicionarTagNova(long idEvento, Tag tag);    // Criação de nova Tag
-    public void removerTag(long idEvento, long idTag);
+    public void adicionarTagExistente(long idObjBase, long idTag); // Adição de Tag existente
+    public void adicionarTagNova(long idObjBase, Tag tag);    // Criação de nova Tag
+    public void removerTag(long idObjBase, long idTag);
 }
