0af813969b6f43c31afccabe194abc1133251492
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-12-11 02:14:33 -0300

    refactor: removendo imports nao usados

--- src/test/java/com/oxentepass/oxentepass/service/EventoServiceTest.java
+++ src/test/java/com/oxentepass/oxentepass/service/EventoServiceTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.Temporal;
-import java.time.temporal.TemporalUnit;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
